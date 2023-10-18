d0c28269b28a6b4b69b58dd5f6f34e0dccbd26d4
Aristides Staffieri <aristides.staffieri@stellar.org>
2023-10-18 15:40:34 -0600

    init repo structure, build steps, docker steps, etc

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# freighter-backend
+# Freighter-Backend
 Freighter's indexer integration layer and general backend
+
+## Prerequisites
+
+You will need
+
+- Node (>=18.0): https://nodejs.org/en/download/
+- Yarn (>=v1.22.5): https://classic.yarnpkg.com/en/docs/install
+
+## Development
+`npm i && npm run start`
+
+## Production build
+`npm run build:prod`
---
+++ webpack.prod.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'production',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  externals: [ nodeExternals() ],
+  module: {
+    rules: [
+      {
+        test: /\.([cm]?ts)$/,
+        use: [
+          'ts-loader',
+        ]
+      }
+    ]
+  }
+}
