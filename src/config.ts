4c21978568d1b4dc8c26e4299a36b2324749b680
Aristides Staffieri <aristides.staffieri@stellar.org>
2024-01-05 10:17:50 -0700

    force env var into boolean in config

--- src/config.ts
+++ src/config.ts
@@ -31,7 +31,9 @@ export function buildConfig(config: Record<string, string>) {
     redisConnectionName:
       config.REDIS_CONNECTION_NAME || process.env.REDIS_CONNECTION_NAME!,
     redisPort: Number(config.REDIS_PORT) || Number(process.env.REDIS_PORT!),
-    useMercury: config.USE_MERCURY || process.env.USE_MERCURY!,
+    useMercury: Boolean(
+      config.USE_MERCURY || process.env.USE_MERCURY! || false
+    ),
   };
 }
 
