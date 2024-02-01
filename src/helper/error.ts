64a286903bcda76dddaa08703d0f773af561dcca
Aristides Staffieri <aristides.staffieri@stellar.org>
2024-02-01 12:18:51 -0700

    capitalize token sim error strings

--- src/helper/error.ts
+++ src/helper/error.ts
@@ -1,7 +1,7 @@
 export const ERROR = {
   ACCOUNT_NOT_SOURCE:
-    "transfer contains authorization entry for a different account",
+    "Transfer contains authorization entry for a different account",
   FOREIGN_CONTRACT_AUTH:
-    "transfer contains authorization entry for another contract",
-  SUB_INVOCATIONS: "transfer contains sub-invocations to another contract",
+    "Transfer contains authorization entry for another contract",
+  SUB_INVOCATIONS: "Transfer contains sub-invocations to another contract",
 };
