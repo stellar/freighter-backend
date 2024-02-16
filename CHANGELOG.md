0bbcb424410ee43ec6c4b92783dd11bd139e401a
Aristides Staffieri <aristides.staffieri@stellar.org>
2024-02-16 10:03:00 -0700

    adds soroban rpc pubnet url, adds toggle for soroban pubnet support, adds first changelog entry, bumps package version

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Release notes - Wallets - Freighter-Backend-1.0.0
+
+Adds support for Soroban RPC on pubnet
+Adds config toggle to enable turning off Soroban on pubnet
--- src/route/index.ts
+++ src/route/index.ts
@@ -36,6 +36,7 @@ export async function initApiServer(
   mercuryClient: MercuryClient,
   logger: Logger,
   useMercury: boolean,
+  useSorobanPublic: boolean,
   register: Prometheus.Registry,
   redis?: Redis
 ) {
@@ -194,10 +195,13 @@ export async function initApiServer(
         ) => {
           const pubKey = request.params["pubKey"];
           const { network, horizon_url, soroban_url } = request.query;
+
+          const skipSorobanPubnet = network === "PUBLIC" && !useSorobanPublic;
           const contractIds = request.query["contract_ids"] || ([] as string[]);
+
           const { data, error } = await mercuryClient.getAccountBalances(
             pubKey,
-            contractIds,
+            skipSorobanPubnet ? [] : contractIds,
             network,
             { horizon: horizon_url, soroban: soroban_url },
             useMercury
@@ -248,6 +252,11 @@ export async function initApiServer(
         ) => {
           const contractId = request.params["contractId"];
           const { network, pub_key, soroban_url } = request.query;
+
+          const skipSorobanPubnet = network === "PUBLIC" && !useSorobanPublic;
+          if (skipSorobanPubnet) {
+            return reply.code(400).send("Soroban has been disabled on pubnet");
+          }
 
           try {
             const data = await mercuryClient.tokenDetails(
