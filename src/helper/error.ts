31a8a536b3e1f7b545d3f1f844d4cfdd2c9ba194
aristides <aristides.staffieri@stellar.org>
2024-02-27 10:28:12 -0700

    Feat/add contract (#36)
    * adds back contractId in rpc schema response
    
    * adds back symbol to rpc balance response

--- src/helper/error.ts
+++ src/helper/error.ts
@@ -3,4 +3,5 @@ export const ERROR = {
     "Transfer contains authorization entry for a different account",
   AUTH_SUB_INVOCATIONS:
     "Transfer authorizes sub-invocations to another contract",
+  FAILED_TO_SIM: "Failed to simulate transaction",
 };
