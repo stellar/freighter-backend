c32eab7e3259cf7a85856089b2b954ae1b53a0fe
Aristides Staffieri <aristides.staffieri@stellar.org>
2024-04-23 16:54:35 -0600

    test new soroban rpc url in preview

--- src/helper/soroban-rpc.ts
+++ src/helper/soroban-rpc.ts
@@ -18,7 +18,7 @@ import { ERROR } from "./error";
 
 const SOROBAN_RPC_URLS: { [key in keyof typeof Networks]?: string } = {
   PUBLIC:
-    "http://soroban-rpc-pubnet-prd.soroban-rpc-pubnet-prd.svc.cluster.local:8000",
+    "http://soroban-rpc-pubnet-dev.soroban-rpc-pubnet-dev.svc.cluster.local:8000",
   TESTNET: "https://soroban-testnet.stellar.org/",
   FUTURENET: "https://rpc-futurenet.stellar.org/",
 };
