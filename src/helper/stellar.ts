17b9d5b13e7b4b8edeb95b5cb79571ceeabbeaa6
Piyal Basu <pbasu235@gmail.com>
2024-06-18 13:21:56 -0400

    Release/1.3.1 (#89)
    * adapt to new mercury schema, no more muxed or byAccountId fields
    
    * default to using sdk v12.1.0 on all networks (#87)
    
    * add is-sac-contract route (#74) (#86)
    
    * add is-sac-contract route
    
    * remove throw from isSacContract helper
    
    * default to using sdk v12.1.0 on all networks
    
    * update sdk-next and contract spec api
    
    ---------
    
    Co-authored-by: Aristides Staffieri <aristides.staffieri@stellar.org>

--- src/helper/stellar.ts
+++ src/helper/stellar.ts
@@ -2,9 +2,7 @@ import * as StellarSdkNext from "stellar-sdk-next";
 import * as StellarSdk from "stellar-sdk";
 
 export const isNextSdk = (networkPassphrase: StellarSdkNext.Networks) =>
-  [StellarSdk.Networks.FUTURENET, StellarSdk.Networks.TESTNET].includes(
-    networkPassphrase
-  );
+  [""].includes(networkPassphrase);
 
 export const getSdk = (networkPassphrase: StellarSdkNext.Networks) =>
   isNextSdk(networkPassphrase) ? StellarSdkNext : StellarSdk;
