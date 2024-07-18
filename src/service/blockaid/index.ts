dd9218a45e667b1bdc45dae9fe8b5897519924bb
aristides <aristides.staffieri@stellar.org>
2024-07-18 15:40:17 -0600

    [FEATURE] adds blockaid scan site service and route (#99)
    * adds blockaid scan site service and route
    
    * adds blockaid mock in test helper
    
    * removes retry on status miss
    
    * adds counter for scan misses
    
    * adds register to blockaid references
    
    * removes unused sentry reference

---
+++ src/service/blockaid/index.ts
@@ -0,0 +1,41 @@
+import Blockaid from "@blockaid/client";
+import Prometheus from "prom-client";
+import { Logger } from "pino";
+
+import { ERROR } from "../../helper/error";
+
+export class BlockAidService {
+  blockAidClient: Blockaid;
+  logger: Logger;
+  scanMissCounter: Prometheus.Counter<"scanMiss">;
+  constructor(
+    blockAidClient: Blockaid,
+    logger: Logger,
+    register: Prometheus.Registry
+  ) {
+    this.blockAidClient = blockAidClient;
+    this.logger = logger;
+    this.scanMissCounter = new Prometheus.Counter({
+      name: "freighter_backend_scan_miss_count",
+      help: "Number of times that a blockaid scan has missed",
+      labelNames: ["scanMiss"],
+      registers: [register],
+    });
+  }
+
+  scanDapp = async (
+    url: string
+  ): Promise<{
+    data: Blockaid.Site.SiteScanResponse | null;
+    error: string | null;
+  }> => {
+    try {
+      const data = await this.blockAidClient.site.scan({ url });
+      return { data, error: null };
+    } catch (error) {
+      this.logger.error(error);
+      this.scanMissCounter.inc();
+      return { data: null, error: ERROR.UNABLE_TO_SCAN_SITE };
+    }
+  };
+}
