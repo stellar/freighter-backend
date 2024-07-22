9bc2c89bdeb016000212947408c93529dd460409
Aristides Staffieri <aristides.staffieri@stellar.org>
2024-07-22 09:18:30 -0600

    adds blockaid scan tx service method and route

--- src/service/blockaid/index.ts
+++ src/service/blockaid/index.ts
@@ -1,8 +1,15 @@
 import Blockaid from "@blockaid/client";
 import Prometheus from "prom-client";
 import { Logger } from "pino";
+import { Networks, TransactionBuilder } from "stellar-sdk";
 
 import { ERROR } from "../../helper/error";
+import { NetworkNames } from "../../helper/validate";
+
+const NetworkNameBlockaid: { [index: string]: "pubnet" | "futurenet" } = {
+  PUBLIC: "pubnet",
+  FUTURENET: "futurenet",
+};
 
 export class BlockAidService {
   blockAidClient: Blockaid;
@@ -36,6 +43,36 @@ export class BlockAidService {
       this.logger.error(error);
       this.scanMissCounter.inc();
       return { data: null, error: ERROR.UNABLE_TO_SCAN_SITE };
+    }
+  };
+
+  scanTx = async (txXdr: string, network: NetworkNames) => {
+    try {
+      const networkPassphrase = Networks[network];
+      const tx = TransactionBuilder.fromXDR(txXdr, networkPassphrase);
+      let source = "";
+      if ("innerTransaction" in tx) {
+        source = tx.innerTransaction.source;
+      } else {
+        source = tx.source;
+      }
+      const body = {
+        chain: NetworkNameBlockaid[network],
+        options: ["validation", "simulation"] as Array<
+          "validation" | "simulation"
+        >,
+        metadata: {
+          type: "wallet" as "wallet",
+          url: "", // TODO: can this be optional, we dont always have it
+        },
+        transactions: [txXdr],
+        account_address: source,
+      };
+      const data = await this.blockAidClient.stellar.transaction.scan(body);
+      return { data, error: null };
+    } catch (error) {
+      this.logger.error(error);
+      return { data: null, error: ERROR.UNABLE_TO_SCAN_TX };
     }
   };
 }
