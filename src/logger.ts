e2776ff41c82ba4b421857fcc6aadc51f6052620
Aristides Staffieri <aristides.staffieri@stellar.org>
2024-07-29 15:25:01 -0600

    redacts log headers and paths

--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,17 @@
 import pino from "pino";
 
+const urlToRedacted = (url: string) => {
+  if (url.includes("/api/v1/account-history")) {
+    return "/api/v1/account-history";
+  }
+  if (url.includes("/api/v1/account-balances")) {
+    return "/api/v1/account-balances";
+  }
+  return url;
+};
+
 const logger = pino({
-  name: "mercury-client-logger",
+  name: "freighter-logger",
   serializers: {
     req: pino.stdSerializers.req,
     err: pino.stdSerializers.err,
@@ -11,6 +21,22 @@ const logger = pino({
     target: "pino-pretty",
     options: {
       colorize: true,
+    },
+  },
+  redact: {
+    paths: [
+      "req.remoteAddress",
+      "req.remotePort",
+      "req.headers.host",
+      "req.headers['user-agent']",
+      "req.headers.url",
+    ],
+    censor: (value: string, path: string[]) => {
+      const _path = path.join(".");
+      if (_path === "req.headers.url") {
+        return urlToRedacted(value);
+      }
+      return "Redacted";
     },
   },
 });
