eabac30f4b95ce4d7be66a3e986094b060b939ad
Aditya Vyas <aditya.vyas@stellar.org>
2025-03-07 10:23:21 -0500

    nit refactor

--- src/service/prices/types.ts
+++ src/service/prices/types.ts
@@ -6,6 +6,7 @@ import {
 } from "redis";
 import TimeSeriesCommands from "@redis/time-series";
 import BigNumber from "bignumber.js";
+
 export type RedisClientWithTS = RedisClientType<
   RedisModules & { ts: typeof TimeSeriesCommands },
   RedisFunctions,
